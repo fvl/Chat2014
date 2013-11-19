dd2cc853400229acefe60a686a3be14271e9cf4c
Lei Feng <lei.feng@oneadvertising.ca>
2013-11-19 16:23:36 +0000

    First commit from Nitrous.io box, rails 4.0.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chat2014::Application.config.secret_key_base = 'fbb566c8f6b39596bcae4f4ba67f1510a9689fb4cd83739ff6055ea252d61b8f751f7bff519ce73a20ff48b4070bbf4e3e057f145f6ad11bfe799f3997f3f896'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Chat2014::Application.config.session_store :cookie_store, key: '_Chat2014_session'
