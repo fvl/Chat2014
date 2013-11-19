5dcfcf5c92f49113d706f4c4149de05c5c2a86a7
Lei Feng <lei.feng@oneadvertising.ca>
2013-11-19 10:43:23 -0500

    First initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chat2014::Application.config.secret_key_base = 'a3ac1f68655361e9750de321867c8620e0c06491905c7bf64873f171e2c25696e05160645f8672c6e8511519aa6a247c31a34575852bc22f2b051a8637013596'
